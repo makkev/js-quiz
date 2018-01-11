78fa30a1a80882ac354cc6be08f406b1f507fd3f
Mak <makke@apac.linkgroup.corp>
2018-01-11 17:02:05 +1100

    add functions

--- main.js
+++ main.js
@@ -6,15 +6,30 @@ const quiz = [
     ["Who created JavaScript", "Brendan Eich"],
 ];
 
-let score = 0;
+function main() {
+    let score = 0;
 
-for (const [question, answer] of quiz) {
-    response = prompt(question);
-    if (response === answer) {
-        alert("correct!");
-        score++;
-    } else {
-        alert("wrong");
+    for (const [question, answer] of quiz) {
+        response = prompt(question);
+        checkAnswer(response, answer)
+    }
+    gameOver();
+
+    function checkAnswer(response, answer) {
+        if (response.toLowerCase() === answer.toLowerCase()) {
+            alert("correct!");
+            score++;
+        } else {
+            alert("wrong");
+        }
+    }
+
+    function gameOver() {
+        return alert(`Game Over. Score: ${score} point${score > 1 ? "s" : ""}`);
     }
 }
-alert(`Game Over. Score: ${score} point${score > 1 ? "s" : ""}`);
+
+
+
+
+// main();
