13e1c1ab82f03404efc43ef70aa36155cec86992
K Mak <kumarajiva08@gmail.com>
2018-01-18 08:05:30 +1100

    add countdown clock

--- main.js
+++ main.js
@@ -44,6 +44,7 @@ const view = {
         this.hide(this.response);
         this.show(this.start);
     },
+    timer: document.querySelector('#timer strong'),
 };
 
 // Game Object
@@ -53,6 +54,8 @@ const game = {
         this.score = 0;
         view.setup();
         this.ask();
+        this.secondsRemaining = 20;
+        this.timer = setInterval(this.countdown, 1000);
     },
     ask(name) {
         if (this.questions.length > 0) {
@@ -67,7 +70,7 @@ const game = {
         event.preventDefault();
         const response = view.response.answer.value;
         const answer = this.question.creator;
-        if (response === answer) {
+        if (response.toLowerCase() === answer.toLowerCase()) {
             view.render(view.result, 'Correct', {'class': 'correct'});
             this.score++;
             view.render(view.score, this.score);
@@ -81,7 +84,15 @@ const game = {
         view.render(view.info, `Game Over. Score: ${this.score} point${this.score > 1 ? "s" : ""}`);
         view.show(view.start);
         view.teardown();
+        clearInterval(this.timer);
     },
+    countdown() {
+        game.secondsRemaining--;
+        view.render(view.timer, game.secondsRemaining);
+        if (game.secondsRemaining <= 0) {
+            game.gameOver();
+        }
+    }
 }
 
 view.start.addEventListener('click', () => game.start(quiz), false);
